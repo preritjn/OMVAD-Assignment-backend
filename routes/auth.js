5d6d8e28718a7f34b45188f440f592d9e51a85fa
preritjn <preritbgr3010@gmail.com>
2025-07-29 20:53:24 +0530

    added something

--- routes/auth.js
+++ routes/auth.js
@@ -16,63 +16,63 @@ const userDataSchema = zod.object({
 router.post('/signup', async (req,res) => {
     const {email, password} = req.body
     try {
-        userDataSchema.safeParse(email,password)
+      userDataSchema.safeParse(email,password)
     }
     catch(err) {
-        return res.status(400).json({message: err})
+      return res.status(400).json({message: err})
     }
 
     const existingUser = await User.findOne({email})
     if(existingUser) {
-        return res.status(400).json({message: "User with this email already exists"})
+      return res.status(400).json({message: "User with this email already exists"})
     }
 
     const hash = bcrypt.hashSync(password, saltRounds)
     try {
-        await User.create({email : email,password: hash})
+      await User.create({email : email,password: hash})
     }
     catch (err) {
-        return res.status(500).json({message: 'Internal server error'})
+      return res.status(500).json({message: 'Internal server error'})
     }
     const token = jwt.sign({ email },process.env.JWT_KEY)
     res.cookie('auth',token,{
-      httpOnly: true
+      sameSite: 'None'
     })
     return res.status(201).json({
-        message: "User Created Succesfully"
+      message: "User Created Succesfully"
     })
 })
 
 router.post('/signin', async (req,res) => {
     const {email, password} = req.body
     try {
-        userDataSchema.safeParse(email,password)
+      userDataSchema.safeParse(email,password)
     }
     catch(err) {
-        return res.status(400).json({message: err})
+      return res.status(400).json({message: err})
     }
     const user = await User.findOne({email})
     if(!user) {
-        return res.status(400).json({message: "User with this email does not exist"})
+      return res.status(400).json({message: "User with this email does not exist"})
     }
     const isValidPassword = bcrypt.compareSync(password,user.password)
     if(isValidPassword) {
-        const token = jwt.sign({ email },process.env.JWT_KEY)
-        res.cookie('auth',token,{
-          httpOnly: true
-        })
-        return res.status(200).json({
-          message : "Signed in Successfully"
-        })
+      const token = jwt.sign({ email },process.env.JWT_KEY)
+      res.cookie('auth',token,{
+        sameSite: 'None'
+      })
+      return res.status(200).json({
+        message : "Signed in Successfully"
+      })
     }
     else {
-        return res.status(400).json({message: "Invalid password"})
+      return res.status(400).json({message: "Invalid password"})
     }
 })
 
 router.post('/signout', (req,res) => {
-    res.clearCookie('auth')
-    return res.status(200).json({message: "Signed out Successfully"})
+  res.clearCookie('auth')
+  return res.status(200).json({message: "Signed out Successfully"})
 })
 
 module.exports = router
